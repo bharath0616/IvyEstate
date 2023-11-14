3530d2666dc3e721950e69f4e64f9cb789f9f12a
Bharath Mudduluru <Bharath Mudduluru@NTPL-D-015.Neovatic.com>
2023-11-14 16:18:27 +0530

    image uplaoding functionality p-2

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,37 +1,85 @@
+/* eslint-disable no-unused-vars */
 import React,{useRef, useState , useEffect} from 'react'
 import { useSelector } from 'react-redux/es/hooks/useSelector';
-import getStorage from 'firebase/storage';
+import {getDownloadURL, getStorage, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase'
+import { ref } from 'firebase/storage';
 export default function Profile() {
   const fileRef=useRef(null)
-  const {currentUser}=useSelector((state)=>state.user)
+  const {currentUser}=useSelector((state)=>state.user);
   const [file,setFile]=useState(undefined)
+  const [filePercentage,setFilePercentage]=useState(0);
+  const [fileUploadError,setFileUploadError]=useState(false);
+  const [formData,setFormData]=useState({});
 
   useEffect(()=>{
     if(file){
-      handleFileUpload(file);
+      handleFileUpload(file);   
     }
   },[file]);
 
   const handleFileUpload=(file)=>{
-    const storage=getStorage(app)
-  }
+    const storage=getStorage(app);
+    const fileName=new Date().getTime() + file.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask=uploadBytesResumable(storageRef,file);
+
+    uploadTask.on('state_changed',
+    (snapshot)=>{
+      const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      setFilePercentage(Math.round(progress));
+    },
+
+    (error)=>{
+      setFileUploadError(true);
+      
+    },
+    
+    ()=>{
+       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>
+        setFormData({ ...formData, avatar: downloadURL}));
+    }
+  )};
+
   return (
     <div className='p-4 max-w-lg mx-auto'>
-    <div>
+    <div> 
       <h1 className='text-3xl font-bold my-8 text-center'>Profile</h1>
+
       <form className='flex flex-col gap-4'>
-      <input type='file' ref={fileRef} hidden/>
+
+      <input type='file' 
+      ref={fileRef} hidden 
+      onChange={(e)=>setFile(e.target.files[0])} 
+      accept='image/*'
+      />
+      
       <img onClick={()=>fileRef.current.click()} 
-      onChange={(e)=>setFile(e.target.files[0])}
-      src={currentUser.avatar} alt='profile'
+      src={formData.avatar || currentUser.avatar } alt='profile'
       className='rounded-full h-22 w-22 object-cover cursor-pointer self-center mt-1' 
-      title='click to update' accept='image/*'/>
-      <input type='text' placeholder='username' id='username' className='border p-4 rounded-lg'/>
-      <input type='email' placeholder='email'id='email' className='border p-4 rounded-lg'/>
-      <input type='password' placeholder='password' id='password'className='border p-4 rounded-lg'/>
+      title='click to update'/>
+      
+      <p className='text-center'>
+            {fileUploadError ? (
+              <span className='text-red-600'>Error uploading image (Image size should be less than 2mb)</span>
+            ) : filePercentage > 0 && filePercentage < 100 ? (
+              <span className='text-gray-800'>{`Uploading ${filePercentage}%`}</span>
+            ) : filePercentage === 100 ? (
+              <span className='text-green-600'>Image successfully updated!</span>
+            ) : (
+              ''
+            )}
+          </p>
+
+      <input type='text' placeholder='username' id='username' autoComplete='username' className='border p-4 rounded-lg'/>
+      <input type='email' placeholder='email' id='email' autoComplete='username' className='border p-4 rounded-lg'/>
+      <input type='password' placeholder='password' id='password' autoComplete='password' className='border p-4 rounded-lg'/>
 
       <button className='bg-gray-900 text-white p-2.5 rounded-lg hover:bg-gray-700  
-      disabled:bg-gray-500 '>Update</button>
+      disabled:bg-gray-500 ' 
+      disabled={filePercentage > 0 && filePercentage < 100}>
+      Update
+      </button>
 
       </form>
       <div className='flex justify-between mt-4'>
