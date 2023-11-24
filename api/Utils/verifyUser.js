c56f56700b03e72c982955db6d886de1ea81c4f3
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-11-24 05:36:41 +0530

    Your commit message

--- api/Utils/verifyUser.js
+++ api/Utils/verifyUser.js
@@ -1,14 +1,15 @@
-import { errorHandler } from "./error"
-import { Jwt } from "jsonwebtoken";
-export const verifyUser=(req,res,next)=>{
-    const token=req.cookies.access_token
+import { errorHandler } from "./error.js";
+import jwt from "jsonwebtoken";  // Use the default import syntax
 
-    if(!token) return next(errorHandler(401,'Unauthorized'));
+export const verifyUser = (req, res, next) => {
+    const token = req.cookies.access_token;
 
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
-        if(err) return next(errorHandler(402,'Forbidden'));
+    if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-        req.user=user;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return next(errorHandler(402, 'Forbidden'));
+
+        req.user = user;
         next();
-    })
-}
+    });
+};
