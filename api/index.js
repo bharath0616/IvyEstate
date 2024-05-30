d5495ee2aeef141e7b0b6f0746d273e23748c70a
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-05-30 23:56:08 +0530

    Client->client

--- api/index.js
+++ api/index.js
@@ -28,10 +28,10 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist'))); //build in case of create react app
+app.use(express.static(path.join(__dirname, '/Client/dist'))); //build in case of create react app
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Client', 'dist', 'index.html'));
 })
 
 app.use((err,req,res,next) => {
